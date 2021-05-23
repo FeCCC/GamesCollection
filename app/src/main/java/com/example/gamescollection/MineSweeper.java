40ffd8df5a53960a3c98cdfb991124e363ccaeff
FeCCC <72006654+FeCCC@users.noreply.github.com>
2021-05-23 20:00:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gamescollection/MineSweeper.java
@@ -0,0 +1,22 @@
+package com.example.gamescollection;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.gamescollection.databinding.ActivityMainBinding;
+
+public class MineSweeper extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mine_sweeper);
+    }
+}
